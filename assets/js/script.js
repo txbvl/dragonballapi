1fe10fd1c29a6dc26fa1e96c5c87ae5825dae5ab
unknown <SSDD@PNAWSA06LC1027.duoc.acad>
2024-05-08 14:59:15 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+import {     
+    loadInitialCharacters , 
+    loadMoreCharacters ,
+    loadOneCharacter,
+    
+} from "./operaciones.js";
+
+import { getCharactersFetchByName } from './fetchs/getCharactersFetchByName.js';
+    
+let isLoading = false;
+
+let nombre = "";
+
+const btnBuscar = document.getElementById("btnBuscar");
+btnBuscar.addEventListener("click" , (e) => {
+    nombre = document.getElementById("nombre").value;
+    e.preventDefault();
+    getCharactersFetchByName(nombre)
+        .then((personaje) => {            
+            loadOneCharacter(personaje);
+        })
+        .catch((error)=> {
+            console.log(`El error es: ${error}`);
+        })
+});
+
+// let nombre = document.getElementById("nombre");
+// nombre.addEventListener("input", (e) => {
+//     e.preventDefault();
+//     getCharactersFetchByName(nombre.value)
+//         .then((personaje)=> {
+//             loadOneCharacter(personaje);
+//         })
+//         .catch((error)=> {
+//             console.log(`El error es: ${error}`);
+//         })
+// })
+
+
+
+window.onload = loadInitialCharacters;
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5 && !isLoading) {
+        loadMoreCharacters();
+    }
+});
